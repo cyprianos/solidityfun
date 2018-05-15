8cba13e89d4d071228719495fd2b724a9c0241c6
Cyprian Gepfert <cyprian.gepfert@codete.com>
2018-05-15 23:15:18 +0200

    run test for Inbox solidity contract; setup mocha runner

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,3 +3,23 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
+const {interface, bytecode} = require('../compile');
+
+
+const web3 = new Web3(ganache.provider());
+let accounts;
+let inbox;
+beforeEach(async ()=>{
+  //get list of all accounts
+  accounts = await web3.eth.getAccounts()
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments:['Hi there!']})
+    .send({from: accounts[0], gas: '1000000'});
+  //use one of this accounts to deploy contract
+});
+
+describe('Inbox', ()=>{
+  it('dummy test',()=>{
+    console.log(inbox)
+  })
+});
